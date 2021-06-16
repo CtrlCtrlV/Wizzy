230e1ac9752176ecae0fd6db65f103d31148c750
ApacheInfinity <79088214+SiD-Inc404@users.noreply.github.com>
2021-06-16 06:30:06 +0000

    BOT IS NOW LIVE

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
 import db
 import plugins
 
+async def tap(args, msObject):
+    await msObject.channel.send(args)
--- main.py
+++ main.py
@@ -7,20 +7,39 @@ import os
 import asyncio
 from utils import *
 import discord
+import commands
 botToken = os.environ['BOTTOKEN']
 bot = discord.Client()
+
+# DECLARING MAIN VARIABLES
 vconsole = []
 LKE = "Client request reached bot servers, awaiting handle"
+incomingRaw = ""
+incomingRawArr = []
+root = ""
+#####################################
 
+# CREATING EVENTS
 @bot.event
 async def on_ready():
     await bot.get_channel(850712941031325720).send("Wiz is online")
+@bot.event
 async def on_message(Message):
-  msg = Message.content
-  await Message.channel.send("Message received: "+msg)
+    if Message.author == bot.user:
+        return
+    msg = Message.content
+    async def send(sendMessage):
+        await Message.channel.send(sendMessage)
 
-  # more code here
-
+    incomingRaw = msg
+    incomingRawArr = msg.split(" ")
+    if incomingRaw.startswith("wiz") or incomingRaw.startswith("orto"):
+        del incomingRawArr[0]
+    root = incomingRawArr[0]
+    if root == "tap":
+        del incomingRawArr[0]
+        await commands.tap(" ".join(incomingRawArr), Message)
+######################################    
 
 bot.run(botToken)
 
